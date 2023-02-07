504c9d268b05fec70de30c211baffe09a7ef5659
Yuliya Mironova <rabkevich15@gmail.com>
2023-02-07 14:23:15 +0100

    Add login component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,27 @@ import React from "react";
 import { Header } from "./header/Header.jsx";
 import { Main } from "./main/Main.jsx";
 import { Footer } from "./footer/Footer.jsx";
+import { Login } from "./login/Login.jsx";
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogin: false
+    }
+  }
+  render() {
+    return (
     <div className="App">
       <Header />
-      <Main />
+        {this.state.isLogin && <Main />}
+        {!this.state.isLogin && <Login />}
       <Footer />
     </div>
   );
+  }
+  
 }
 
 export default App;
---
+++ src/login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Login.module.css";
+
+class Login extends React.Component {
+  render() {
+    return <main className={styles.login}>        
+        <p>Login</p>
+      </main>
+  }
+}
+
+export { Login }
